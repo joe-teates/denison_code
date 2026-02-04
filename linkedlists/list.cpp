a8e6ab0bb6de61b555367cf432d10ba99a717f9e
Joe <94333054+joe-teates@users.noreply.github.com>
2026-02-04 11:14:58 -0500

    Added comments for implementation files

--- linkedlists/list.cpp
+++ linkedlists/list.cpp
@@ -1,34 +1,15 @@
+// Joe Teates and Emma Salamon
+// This file contains the code implementation of helper functions for a doubly linked list. 
+
 #include "list.hpp"
 #include <iostream>
 using namespace std;
 
-
-// class Node{
-
-//     public:
-
-//     int key;
-//     int value;
-//     Node *prev;
-//     Node *next;
-
-//     Node(int key, int value) : key(key), value(value), prev(nullptr), next(nullptr) {}
-
-// };
-
-// class MyList{
-
-//     public:
-
-//     Node *head;
-
-//     MyList() : head(nullptr) {}
-
-// };
-
-
 Node* listSearch(MyList& list, int& key) {
+// listSearch iterates through a linked list for a particular node by checking the key property
+    // Gets node to start from
     Node* current_node = list.head;
+    // Check if the key matches, if not jump to next key and repeat until at end of list
     while(current_node != nullptr && current_node->key != key){
         current_node = current_node->next;
     }
@@ -37,34 +18,47 @@ Node* listSearch(MyList& list, int& key) {
 }
 
 void listPrepend(MyList& list, Node& node) {
+// listPrepend adds a node at the start of a list, shifting any existing elements one index to the right/back
+    // set next and prev pointers of new node
     node.next = list.head;
     node.prev = nullptr;
+    // if there are existing elements, update prev property for old head
     if (list.head != nullptr) {
         list.head->prev = &node;
     }
+    // set new head for the list
     list.head = &node;
     return;
 }
 
 void listInsert(Node& node_x, Node& node_y) {
+// This function adds a new node (node_x) to the list directly after node_y
+    // set next and prev pointers of new node
     node_x.next = node_y.next;
     node_x.prev = &node_y;
+    // if there is an element after node_y, update its prev pointer
     if (node_y.next != nullptr) {
         node_y.next->prev = &node_x;
     }
+    // update node_y's next pointer
     node_y.next = &node_x;
     return;
 }
 
 void listDelete(MyList& list, Node& node) {
+// This function deletes a node (node) from the list
+    // if an element exists before node, make it's next skip over this node
     if (node.prev != nullptr) {
         node.prev->next = node.next;
     } else {
+        // else this element is the head and so it needs to be updated
         list.head = node.next;
     }
+    // check if an element exists after node, if it does update its prev pointer
     if (node.next != nullptr) {
         node.next->prev = node.prev;
     }
+    // deallocate - should add a cleanup function but not in header
     delete &node;
     return;
 }
--- linkedlists/single_list.cpp
+++ linkedlists/single_list.cpp
@@ -1,34 +1,16 @@
+// Joe Teates and Emma Salamon
+// This file contains the code implementation of helper functions for a single linked list. 
+
+
 #include "list.hpp"
 #include <iostream>
 using namespace std;
 
-
-// class Node{
-
-//     public:
-
-//     int key;
-//     int value;
-//     Node *prev;
-//     Node *next;
-
-//     Node(int key, int value) : key(key), value(value), prev(nullptr), next(nullptr) {}
-
-// };
-
-// class MyList{
-
-//     public:
-
-//     Node *head;
-
-//     MyList() : head(nullptr) {}
-
-// };
-
-
 Node* listSearch(MyList& list, int& key) {
+// listSearch iterates through a linked list for a particular node by checking the key property
+    // Gets node to start from
     Node* current_node = list.head;
+    // Check if the key matches, if not jump to next key and repeat until at end of list
     while(current_node != nullptr && current_node->key != key){
         current_node = current_node->next;
     }
@@ -36,36 +18,41 @@ Node* listSearch(MyList& list, int& key) {
 }
 
 void listPrepend(MyList& list, Node& node) {
+// listPrepend adds a node at the start of a list, shifting any existing elements one index to the right/back
+    // set next pointer of new node
     node.next = list.head;
+    // update the list head
     list.head = &node;
     return;
 }
 
 void listInsert(Node& node_x, Node& node_y) {
+// This function adds a new node (node_x) to the list directly after node_y
+    // set next pointer of new node
     node_x.next = node_y.next;
+    // update node_y's next pointer
     node_y.next = &node_x;
     return;
 }
 
 void listDelete(MyList& list, Node& node) {
-    // if (node.prev != nullptr) {
-    //     node.prev->next = node.next;
-    // } else {
-    //     list.head = node.next;
-    // }
-    // if (node.next != nullptr) {
-    //     node.next->prev = node.prev;
-    // }
-    // delete &node;
+// This function deletes a node (node) from the list. It does this by iterating through the list to 
+// find the desired node and the previous node and updates those accordingly
+    // get node to start from
     Node* current_node = list.head;
     Node* last_node = nullptr;
-
-    while(current_node != nullptr) {
+    bool found_node = false;
+    // iterate through list until it finds the node to delete or hits end of list
+    while(current_node != nullptr && !found_node) {
         if (current_node->key == node.key) {
+            // found the node to delete, update next property of previous node to skip over the node
             last_node->next = current_node->next;
+            // deallocate
             delete &node;
+            // end loop
+            found_node = true;
         }
-
+        // iterate the last and current nodes
         last_node = current_node;
         current_node = current_node->next;
     }
